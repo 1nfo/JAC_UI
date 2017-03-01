0adc9d632000e73cadd1bdfcf33cea7d2157e8d7
vincent <shilzhao@paypal.com>
2017-03-01 12:08:55 -0500

    std out flush out

--- main.py
+++ main.py
@@ -1,45 +1,146 @@
-from flask import Flask,render_template,request,jsonify
-import json
+from flask import Flask,render_template,request,jsonify,session,redirect
+from flask_socketio import SocketIO, emit, disconnect
+from werkzeug.utils import secure_filename
+import json,os,io,sys
 import JmeterAwsConf as JAC
 
+
+# old part before add socketio
 app  = Flask(__name__)
-taskMngr = None
-
-
-@app.route("/")
-def hp():
-	title = "Jmeter Cloud Testing"
-	paragraph = ['']
-	return render_template("index.html",title=title,paragraph=paragraph)
-
-
-@app.route("/post/taskName",methods = ['POST'])
-def createTask():
-	taskName = request.form["taskName"]
-	taskMngr = JAC.TaskManager(taskName,0,config=JAC.CONFIG)
-	return "from server: "+taskName
-
-
-@app.route("/post/slaveNum",methods = ['POST'])
-def setSlaveNum():
-	var = request.form["slaveNum"]
-	return "from server: "+var
-
-
-@app.route("/uploadFiles",methods = ['POST'])
-def uploadFiles():
-	print(request.files.getlist("file"))
-	return json.dumps({"success":True}), 200
-
-
 app.config.update(
 	DEBUG=True,
 	TEMPLATES_AUTO_RELOAD=True,
 	UPLOAD_FOLDER='uploads/'
 )
 
+# new part 
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
+
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
+taskMngrs = {}
+
+
+def flushPasuse():
+	socketio.sleep(.01)
+
+jredirector = JAC.Redirector(pauseFunc=flushPasuse)
+
+
+def background_thread():
+	count = 0
+	while True:
+		# count+=1
+		socketio.sleep(.01)
+		
+		socketio.emit('redirect',
+					{'msg': jredirector.flush().replace("\n","<br/>")},
+					namespace='/redirect')
+
+@app.route("/")
+def index():
+	return redirect("/admin")
+
+
+@app.route("/<user>")
+def user(user):
+	title = "Jmeter Cloud Testing"
+	paragraph = ['']
+	return render_template("index.html",title=title,paragraph=paragraph,async_mode=socketio.async_mode)
+
+
+@app.route("/post/taskName",methods = ['POST'])
+def createTask():
+	taskName = request.form["taskName"]
+	taskID = request.form["taskID"]
+	createOrNot = int(request.form["create"])
+	taskMngr = JAC.TaskManager(config=JAC.CONFIG)
+	successOrNot = False;
+	with jredirector:
+		try:
+			taskMngr.startTask(taskName)
+			successOrNot = True
+		except Exception as exception:
+			if createOrNot:
+				print(exception.args[0])
+				print(exception.args[1])
+				# print(exception.args[2])
+			else:
+				print("Resuming "+exception.args[1][0])
+				taskMngr.startTask(taskName,exception.args[1][0])
+				successOrNot = True
+		if successOrNot:
+			taskID = taskMngr.instMngr.taskID
+			taskMngrs[taskID] = taskMngr
+			taskMngr.instMngr.mute()
+			taskMngr.connMngr.mute()
+	return json.dumps({"taskID":taskID}),200 if successOrNot else 400
+
+
+@app.route("/post/slaveNum",methods = ['POST'])
+def setSlaveNum():
+	num = int(request.form["slaveNum"])
+	taskID = request.form["taskID"]
+	with jredirector:
+		taskMngrs[taskID].setSlaveNumber(num)
+		socketio.sleep(3)
+	return "from server: "+str(num)
+
+
+@app.route("/uploadFiles",methods = ['POST'])
+def uploadFiles():
+	taskID = request.form["taskID"]
+	files = request.files.getlist("file")
+	os.system("cd %s && mkdir %s"%(app.config['UPLOAD_FOLDER'],taskID))
+	for file in files:
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER']+taskID+"/", filename))
+	with jredirector:
+		taskMngrs[taskID].setUploadDir(os.getcwd()+"/"+app.config['UPLOAD_FOLDER']+taskID)
+	return json.dumps({"success":True}), 200
+
+
+@app.route("/post/run",methods = ["POST"])
+def runTest():
+	taskID = request.form["taskID"]
+	jmxName = request.form["jmx_name"]
+	taskMngr = taskMngrs[taskID]
+	with jredirector:
+		taskMngrs[taskID].setupInstances()
+		if taskMngr.checkStatus(): 
+			taskMngr.refreshConnections()
+			taskMngr.uploadFiles()
+			taskMngr.updateRemotehost()
+			taskMngr.startSlavesServer()
+			taskMngr.runTest(jmxName,"output.csv")
+			taskMngr.stopSlavesServer()	
+	return json.dumps({"success":True}), 200
+
+
+@app.route("/post/cleanup",methods = ["POST"])
+def cleanup():
+	taskID = request.form["taskID"]
+	with jredirector:
+		taskMngrs[taskID].cleanup()
+	del taskMngrs[taskID]
+	return json.dumps({"success":True}), 200
+
+
+@socketio.on('connect', namespace='/redirect')
+def test_connect():
+    global thread
+    if thread is None:
+        thread = socketio.start_background_task(target=background_thread)
+    emit('redirect', {'msg': 'Connected\n', 'count': 0})
+
 
 
 if __name__ == "__main__":
-    app.run()
+    #app.run()
+    socketio.run(app, debug=True)
 
--- static/js/btnEvent.js
+++ static/js/btnEvent.js
@@ -1,11 +1,15 @@
+var GLOBAL_JAC_taskID = '';
+
 $("#btn_createTask").click(function(e){
 	document.getElementById("InputBlock_task").style.display = "block";
-	document.getElementById("InputRow_taskID").style.display = "none";
+	// document.getElementById("InputRow_taskID").style.display = "none";
+	task_to_create = 1;
 })
 
 $("#btn_resumeTask").click(function(e){
 	document.getElementById("InputBlock_task").style.display = "block";
-	document.getElementById("InputRow_taskID").style.display = "block";
+	// document.getElementById("InputRow_taskID").style.display = "block";
+	task_to_create = 0;
 })
 
 $("#btn_taskConfirm").click(function(e){
@@ -13,9 +17,19 @@ $("#btn_taskConfirm").click(function(e){
 		alert("Name needs to be letters only");
 	}
 	else {
-		var res = $.post("/post/taskName",{"taskName":$("#jac_taskName").val()})
-		console.log(res);
-		document.getElementById("InputBlock_slaveNem").style.display = "block";
+		var res = $.post("/post/taskName",{
+			"taskName":$("#jac_taskName").val(),
+			"taskID":$("#jac_taskID").val(),
+			"create":task_to_create
+		},function(data){
+			data = JSON.parse(data)
+			GLOBAL_JAC_taskID = data["taskID"]
+			// buff = data["buffer"].replace("\n","<br/>")
+			// console.log(buff)
+			// $("#output").append(buff)
+			document.getElementById("InputBlock_slaveNem").style.display = "block";
+			document.getElementById("cleaup_btn_div").style.display = "block";
+		})
 	}
 })
 
@@ -24,16 +38,19 @@ $("#btn_setSlaveNum").click(function(e){
 		alert("Number must be greater than 0");
 	}
 	else{
-		var res = $.post("/post/slaveNum",{"slaveNum":$("#jac_slaveNum").val()})
-		console.log(res);
-		document.getElementById("InputBlock_uploadFiles").style.display = "block";
+		var res = $.post("/post/slaveNum",
+			{"slaveNum":$("#jac_slaveNum").val(),"taskID":GLOBAL_JAC_taskID},
+			function(data){
+				// console.log(data)
+				document.getElementById("InputBlock_uploadFiles").style.display = "block";
+			})
 	}
 })
 
 $("#btn_uploadTask").click(function(e){
 	var filesList = $("#jac_uploadFiles").prop("files")
 	var form_data = new FormData();
-	gata = null
+	form_data.append("taskID",GLOBAL_JAC_taskID)
 	$.each(filesList,function(i,d){form_data.append("file",d)})
 	$.ajax({
 		url:"/uploadFiles",
@@ -43,19 +60,32 @@ $("#btn_uploadTask").click(function(e){
 		processData: false,
 		data: form_data,
 		type: "post",
-		success:function(data){alert("succeed")},
+		success:function(data){
+			alert("succeed");
+			document.getElementById("InputBlock_execJMX").style.display = "block";
+		},
 		error:function(err){alert("failed")}
 	})
-
-	document.getElementById("InputBlock_execJMX").style.display = "block";
 })
 
 $("#btn_runTask").click(function(e){
-	console.log(e)
+	var jmx_to_run = $("#jac_JMXName").val()
+	$.post("/post/run",{"jmx_name":jmx_to_run,"taskID":GLOBAL_JAC_taskID},
+		function(data){
+			console.log(data)
+		})
 })
 
 $("#btn_cleanupTask").click(function(e){
-	console.log(e)
+	$.post("/post/cleanup",{"taskID":GLOBAL_JAC_taskID},
+		function(data){
+			console.log(data)
+			document.getElementById("cleaup_btn_div").style.display = "none";
+			document.getElementById("InputBlock_slaveNem").style.display = "none";
+			document.getElementById("InputBlock_uploadFiles").style.display = "none";
+			document.getElementById("InputBlock_execJMX").style.display = "none";
+			
+		})
 })
 
 $("#btn_clear").click(function(){
