2614fd2bf4bb3efb77d15c8e4b7dcfcbc2a12bc3
vincent <shilzhao@paypal.com>
2017-05-05 14:44:35 -0400

    login opt

--- server/PaydiantAuthentication.py
+++ server/PaydiantAuthentication.py
@@ -6,6 +6,7 @@ class PaydiantAuthentication(object):
         self.server = Server("cse1-ad1.pydt.lan",get_info=ALL)
 
     def verify(self,username,password):
+        if not username: return False
         try:
             Connection(self.server, username+"@pydt.lan", password, auto_bind=True)
             return True
