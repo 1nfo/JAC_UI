69e516c1c333153a787c6cf26b4c2c9ea9514ad1
Shile Zhao <shilzhao@paypal.com>
2017-08-22 12:54:36 -0400

    Revert "update docker to run webpack before sever start"

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
 # this is shortcut for docker
-
-# need to compile JavaScript
-webpack
-# start redis server
 redis-server --daemonize yes
-# start application
 python3 /webapp/main.py server
